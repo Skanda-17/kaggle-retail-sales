77db0d7797652e9c91f6035eba14d39668dc2c0e
Skanda-17 <162621676+Skanda-17@users.noreply.github.com>
2025-04-26 00:46:05 +0530

    Update retail_sales_kaggle.py

--- retail_sales_kaggle.py
+++ retail_sales_kaggle.py
@@ -104,7 +104,7 @@ class RetailSalesPredictor:
             plt.xlabel('Date')
             plt.ylabel('Total Amount')
             plt.grid(True)
-            plt.savefig('static/sales_data_visualization.png')
+            plt.savefig(os.path.join('static/sales_data_visualization.png'))
 
             plt.close()
             
@@ -132,7 +132,7 @@ class RetailSalesPredictor:
                 plt.plot(decomposition.resid)
                 plt.title('Residual')
                 plt.tight_layout()
-                plt.savefig('static/seasonal_decomposition.png')
+                plt.savefig(os.path.join('static/seasonal_decomposition.png'))
 
                 plt.close()
             
